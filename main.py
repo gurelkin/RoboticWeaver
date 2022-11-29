e0e02ac207c2c1be6e262ab314849108c0e724d1
Gur Elkin <93826819+gurelkin@users.noreply.github.com>
2022-11-29 18:17:13 +0200

    29/11
    Improved nails location and added a drawing option (needs to pip install skimage - google it)

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
+from skimage.draw import line_aa
+
 import preprocessing as pre
 
-
 if __name__ == '__main__':
-    # plt.imshow(pre.read_image("MonLisa.jpg"), cmap='gray')
+    # plt.imshow(pre.read_image("MonaLisa.jpg"), cmap='gray')
     # plt.show()
+
     shape = (1000, 1000)
-    k = 500
-    locs = pre.nails_locations(shape, k)
+    k = 52
+    locs = pre.nails_locations_new(shape, k)
     canvas = np.zeros(shape)
-    for n in locs:
-        canvas[n] = 1
+
+    for nail1 in locs:
+        for nail2 in locs:
+            rr, cc, val = line_aa(*nail1, *nail2)
+            val *= 0.5
+            canvas[rr, cc] += val
+    for index in np.ndindex(canvas.shape):
+        canvas[index] = min(canvas[index], 1)
+        canvas[index] = 1-canvas[index]
+
     plt.imshow(canvas, cmap='gray')
     plt.show()
 
--- preprocessing.py
+++ preprocessing.py
@@ -23,14 +23,16 @@ def nails_locations(shape: tuple, k: int) -> np.array:
     rows, cols = shape
     perimeter = 2*rows + 2*cols
     gap = int(np.floor(perimeter/k))
-    locations = []
+    # Put a nail in every corner
+    locations = [(0, 0), (rows-1, 0), (0, cols-1), (rows-1, cols-1)]
+    k -= 4
     # Set nails locations on the left and right of the images
-    for i in range(0, rows, gap):
+    for i in range(gap, rows, gap):
         locations.append((i, 0))
         locations.append((i, cols-1))
         k -= 2
     # Set nails locations on the top and bottom of the images
-    for j in range(gap, cols, gap):
+    for j in reversed(range(gap, cols, gap)):
         locations.append((0, j))
         locations.append((rows-1, j))
         k -= 2
@@ -38,4 +40,27 @@ def nails_locations(shape: tuple, k: int) -> np.array:
             break
     return set(locations)
 
+
+def nails_locations_new(shape: tuple, k: int) -> list[tuple[int, int]]:
+    """
+    :param shape: Shape of the image
+    :param k: Number of nails to be placed
+    :return: The indices of the nails' locations on the image
+    """
+    rows, cols = shape
+    border_pixels = []
+    # Trace a continuous sequence of border pixels
+    for i in range(rows-1):
+        border_pixels.append((i, 0))
+    for j in range(cols-1):
+        border_pixels.append((rows-1, j))
+    for i in reversed(range(1, rows)):
+        border_pixels.append((i, cols-1))
+    for j in reversed(range(1, cols)):
+        border_pixels.append((0, j))
+    # Find the gap between each nail and choose the pixels with this gap between them
+    perimeter = len(border_pixels)
+    gap = int(np.floor(perimeter / k))
+    chosen_indices = range(0, perimeter, gap)
+    return [border_pixels[i] for i in chosen_indices]
 
