bfc9387616d17efee8b5c2c871e61e90a641c6aa
uriziv17 <74453815+uriziv17@users.noreply.github.com>
2023-05-27 18:59:30 +0300

    robot files from email

--- robot/robot_files/MoveZ.m
+++ robot/robot_files/MoveZ.m
@@ -4,19 +4,16 @@ function MoveZ(z)
 
 Com_h = evalin('base','Com_h');
 if (Com_h==0)
+   if (z < -455) 
+       error('Z out of proper range');
+   end
 %   co shai1202  sensitivity = 50; % Higher is less sensitive
-     vel = 5;%Global_params('speed');          % Set velocity
-     
-     P = GetFullPos();
-      
-%     Number_of_moves = floor(abs((z-P(3))/sensitivity));
-%     Z_new = P(3);
-%     for i=1:Number_of_moves
-%         if (z>P(3)) Z_new = Z_new + sensitivity; end
-%         if (z<P(3)) Z_new = Z_new - sensitivity; end
-%         MoveRobot(P(1),P(2),Z_new,P(4),P(5),P(6),'ROBOT',vel);
-%  end co shai1202   end
-    MoveRobot(P(1),P(2),z,P(4),P(5),P(6),'ROBOT',vel);
+    vel = 5;%Global_params('speed');          % Set velocity  
+    P = GetFullPos(); 
+    pos = GetCurrPos();
+    x = pos(1) + 550;
+    y = pos(2) - 250;
+    MoveRobot(x,y,z,P(4),P(5),P(6),'ROBOT',vel);
     pause(0.2);
 else
     disp('Communication is not initialized correctly. Please apply "Com_h = Init()" first until Com_h==0.');
